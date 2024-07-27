aa11273401435b86c649fc08ba3e30a7bc125dc1
ÖMER FARUK YILMAZ <omerfaruk2441@gmail.com>
2024-07-27 23:43:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function totalCharCount(arr) {
+  /* kodlar buraya */
+  let text = arr.join('');
+  return text.length;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = totalCharCount;
